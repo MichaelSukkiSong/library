69fc69daab9bf755f218767fc2010f46ef90ef1f
MichaelSukkiSong <sa77irak@gmail.com>
2024-06-24 17:58:13 -0700

    add: added htmlFor and id in label and input tags, added aria-live for screen readers

--- client/src/components/BookForm.js
+++ client/src/components/BookForm.js
@@ -21,57 +21,77 @@ const BookForm = ({
       <h1 className="book-form__title">{FormTitle}</h1>
       <form onSubmit={handleSubmit} autocomplete="off">
         <div className="book-form__field">
-          <label className="book-form__label">Title</label>
+          <label className="book-form__label" htmlFor="title">
+            Title
+          </label>
           <input
             className="book-form__input"
             name="title"
+            id="title"
             placeholder="Title"
             value={formValue.title}
             onChange={handleChange}
           />
           {errors.title && (
-            <div className="book-form__error">{errors.title}</div>
+            <div className="book-form__error" aria-live="polite">
+              {errors.title}
+            </div>
           )}
         </div>
         <div className="book-form__field">
-          <label className="book-form__label">Author</label>
+          <label className="book-form__label" htmlFor="author">
+            Author
+          </label>
           <input
             className="book-form__input"
             name="author"
+            id="author"
             placeholder="Author"
             value={formValue.author}
             onChange={handleChange}
           />
           {errors.author && (
-            <div className="book-form__error">{errors.author}</div>
+            <div className="book-form__error" aria-live="polite">
+              {errors.author}
+            </div>
           )}
         </div>
         <div className="book-form__field">
-          <label className="book-form__label">Year Published</label>
+          <label className="book-form__label" htmlFor="yearPublished">
+            Year Published
+          </label>
           <input
             className="book-form__input"
             type="number"
             name="yearPublished"
+            id="yearPublished"
             placeholder="Year Published"
             value={formValue.yearPublished}
             onChange={handleChange}
             min="1"
           />
           {errors.yearPublished && (
-            <div className="book-form__error">{errors.yearPublished}</div>
+            <div className="book-form__error" aria-live="polite">
+              {errors.yearPublished}
+            </div>
           )}
         </div>
         <div className="book-form__field">
-          <label className="book-form__label">Genre</label>
+          <label className="book-form__label" htmlFor="genre">
+            Genre
+          </label>
           <input
             className="book-form__input"
             name="genre"
+            id="genre"
             placeholder="Genre"
             value={formValue.genre}
             onChange={handleChange}
           />
           {errors.genre && (
-            <div className="book-form__error">{errors.genre}</div>
+            <div className="book-form__error" aria-live="polite">
+              {errors.genre}
+            </div>
           )}
         </div>
         <div className="book-form__button-section">
